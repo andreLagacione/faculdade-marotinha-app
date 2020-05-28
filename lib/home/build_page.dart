9f774e07b7edbc05697a582f5455d26b6f377260
Lagacione <lagacioneandre@gmail.com>
2020-05-28 12:47:54 -0300

    add button for navigate to create page

--- lib/home/build_page.dart
+++ lib/home/build_page.dart
@@ -6,14 +6,38 @@ class BuildPage extends StatelessWidget {
   final String title;
   final StatelessWidget contentPage;
   final PageController pageController;
+  final Widget createPage;
 
-  BuildPage(this.title, this.contentPage, this.pageController);
+  BuildPage(this.title, this.contentPage, this.pageController, this.createPage);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(this.title),
+        elevation: 0,
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: ClipOval(
+              child: Material(
+                color: Colors.transparent, // button color
+                child: InkWell(
+                  splashColor: Colors.black.withAlpha(0), // inkwell color
+                  child: SizedBox(
+                      width: 40,
+                      height: 40,
+                      child: Icon(Icons.add)),
+                  onTap: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => this.createPage)
+                    );
+                  },
+                ),
+              ),
+            ),
+          )
+        ],
       ),
       body: this.contentPage,
       drawer: Sidebar(this.pageController),
