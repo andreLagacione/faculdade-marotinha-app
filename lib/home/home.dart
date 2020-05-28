0e74d86b7568cdab31dad45ad39c305a0a2312a3
Lagacione <lagacioneandre@gmail.com>
2020-05-28 12:48:28 -0300

    import create pages

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,9 +1,15 @@
+import 'package:faculdademarotinhaapp/alunos/create.dart';
 import 'package:faculdademarotinhaapp/alunos/lista.dart';
+import 'package:faculdademarotinhaapp/boletins/create.dart';
 import 'package:faculdademarotinhaapp/boletins/lista.dart';
+import 'package:faculdademarotinhaapp/cursos/create.dart';
 import 'package:faculdademarotinhaapp/cursos/lista.dart';
 import 'package:faculdademarotinhaapp/home/build_page.dart';
+import 'package:faculdademarotinhaapp/materias/create.dart';
 import 'package:faculdademarotinhaapp/materias/lista.dart';
+import 'package:faculdademarotinhaapp/professores/create.dart';
 import 'package:faculdademarotinhaapp/professores/lista.dart';
+import 'package:faculdademarotinhaapp/turmas/create.dart';
 import 'package:faculdademarotinhaapp/turmas/lista.dart';
 import 'package:flutter/material.dart';
 
@@ -17,12 +23,12 @@ class Home extends StatelessWidget {
       controller: _pageController,
       physics: NeverScrollableScrollPhysics(),
       children: <Widget>[
-        BuildPage('Alunos', AlunoLista(), this._pageController),
-        BuildPage('Boletins', BoletimLista(), this._pageController),
-        BuildPage('Cursos', CursosLista(), this._pageController),
-        BuildPage('Matérias', MateriasLista(), this._pageController),
-        BuildPage('Professores', ProfessoresLista(), this._pageController),
-        BuildPage('Turmas', TurmasLista(), this._pageController),
+        BuildPage('Alunos', AlunoLista(), this._pageController, AlunoCreate()),
+        BuildPage('Boletins', BoletimLista(), this._pageController, BoletimCreate()),
+        BuildPage('Cursos', CursosLista(), this._pageController, CursosCreate()),
+        BuildPage('Matérias', MateriasLista(), this._pageController, MateriasCreate()),
+        BuildPage('Professores', ProfessoresLista(), this._pageController, ProfessoresCreate()),
+        BuildPage('Turmas', TurmasLista(), this._pageController, TurmasCreate()),
       ],
     );
   }
