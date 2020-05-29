d276be713e3fb87fd984c69eb5edbcb529f43d9e
Lagacione <lagacioneandre@gmail.com>
2020-05-29 19:18:28 -0300

    change to use circle buttom component

--- lib/home/build_page.dart
+++ lib/home/build_page.dart
@@ -1,3 +1,4 @@
+import 'package:faculdademarotinhaapp/commons/circleButton.dart';
 import 'package:faculdademarotinhaapp/sidebar/sidebar.dart';
 import 'package:flutter/material.dart';
 
@@ -19,23 +20,19 @@ class BuildPage extends StatelessWidget {
         actions: <Widget>[
           Padding(
             padding: EdgeInsets.all(10),
-            child: ClipOval(
-              child: Material(
-                color: Colors.transparent, // button color
-                child: InkWell(
-                  splashColor: Colors.black.withAlpha(0), // inkwell color
-                  child: SizedBox(
-                      width: 40,
-                      height: 40,
-                      child: Icon(Icons.add)),
-                  onTap: () {
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => this.createPage)
-                    );
-                  },
-                ),
-              ),
-            ),
+            child: CircleButton(
+              buttonColor: Colors.transparent,
+              inkwelColor: Colors.black.withAlpha(0),
+              size: 40,
+              icon: Icons.add,
+              iconColor: null,
+              iconSize: 22,
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => this.createPage)
+                );
+              },
+            )
           )
         ],
       ),
