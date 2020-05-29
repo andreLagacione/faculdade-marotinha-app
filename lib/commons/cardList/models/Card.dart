1792113516bd27d5b3237c6a26cb7caa2aba3a90
Lagacione <lagacioneandre@gmail.com>
2020-05-29 20:24:35 -0300

    create nem modal

---
+++ lib/commons/cardList/models/Card.dart
@@ -0,0 +1,7 @@
+import 'package:faculdademarotinhaapp/commons/cardList/models/ContentCard.dart';
+
+class Card {
+  final String title;
+  final List<ContentCard> content;
+  Card(this.title, this.content);
+}
